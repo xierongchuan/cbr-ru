d244327e37777eeaa75d78fb500c98bfe1fe1f87
黑鱼 <xierongchuan@tutamail.com>
2026-04-21 23:56:31 +0500

    add: new currencies table

---
+++ database/migrations/2026_04_21_184350_create_currencies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->char('char_code', 3)->unique();
+            $table->string('name')->unique();
+            $table->integer('nominal');
+            $table->boolean('is_fetched')->default(true);
+            $table->boolean('is_visible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
