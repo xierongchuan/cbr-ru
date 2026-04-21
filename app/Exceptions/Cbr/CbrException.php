462a3b0229bd11774fde6f359c2ce80a71a59c40
黑鱼 <xierongchuan@tutamail.com>
2026-04-22 02:14:30 +0500

    feat: implement CBR currency rate synchronization service with client, parser, and console command support

---
+++ app/Exceptions/Cbr/CbrException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Cbr;
+
+use Exception;
+
+class CbrException extends Exception
+{
+}
---
+++ app/Exceptions/Cbr/CbrTimeoutException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Cbr;
+
+class CbrTimeoutException extends CbrException
+{
+}
